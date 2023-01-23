31b7e7de7228c46b4d3bde7810cb38c5e9af66bd
Luana Melissa <luanamelissaprogrammer@gmail.com>
2023-01-23 17:43:28 -0300

    7: UserResource findById 👩

--- src/main/java/io/github/luaprogrammer/api/resources/UserResource.java
+++ src/main/java/io/github/luaprogrammer/api/resources/UserResource.java
@@ -1,6 +1,8 @@
 package io.github.luaprogrammer.api.resources;
 
 import io.github.luaprogrammer.api.model.User;
+import io.github.luaprogrammer.api.services.impl.UserServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,11 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserResource {
 
+    private final UserServiceImpl service;
+
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(new User(1, "Zoe", "zoe@email.com", "123"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
