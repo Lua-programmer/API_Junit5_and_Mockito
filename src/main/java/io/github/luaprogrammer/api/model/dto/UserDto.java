41ab73d103719df39b8ecea03b0ee81b02a68c8e
Luana Melissa <luanamelissaprogrammer@gmail.com>
2023-01-23 18:27:55 -0300

    12: UserDto class ✔

---
+++ src/main/java/io/github/luaprogrammer/api/model/dto/UserDto.java
@@ -0,0 +1,17 @@
+package io.github.luaprogrammer.api.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+}
