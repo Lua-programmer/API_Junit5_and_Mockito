29f3f6ac25aa0a460310f6bdd80e8f37ead20a0e
Luana Melissa <luanamelissaprogrammer@gmail.com>
2023-01-23 16:50:22 -0300

    2: User Class 👩

---
+++ src/main/java/io/github/luaprogrammer/api/model/User.java
@@ -0,0 +1,40 @@
+package io.github.luaprogrammer.api.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        User user = (User) o;
+        return id != null && Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
